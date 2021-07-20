1503ba3e5956476c9158334fd62db6668f3dcd23
Miroslav <miroslav_995@abv.bg>
2021-07-20 23:08:19 +0300

    Adding on Arrays - More Exercise

---
+++ C# Fundamentals - 2021/Arrays - More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int lines = int.Parse(Console.ReadLine());
+			char[] vowel = { 'a', 'e', 'i', 'o', 'y', 'u' };
+			int vowelsum = 0;
+			int cantvowel = 0;
+			int[] saveScores = new int[lines];
+			for (int i = 0; i < lines; i++)
+			{
+				string random = Console.ReadLine();
+				int sum = 0;
+				for (int j = 0; j < random.Length; j++)
+				{
+					char symbol = Convert.ToChar(random[j]);
+					switch (symbol)
+					{
+						case 'a':
+						case 'A':
+						case 'o':
+						case 'O':
+						case 'e':
+						case 'E':
+						case 'i':
+						case 'I':
+						case 'u':
+						case 'U':
+							sum += symbol * random.Length;
+							break;
+						default:
+							sum += symbol / random.Length;
+							break;
+					}
+				}
+				saveScores[i] = sum;
+				sum = 0;
+
+			}
+			Array.Sort(saveScores);
+			foreach (var elements in saveScores)
+			{
+				Console.WriteLine(elements);
+			}
+		}
+	}
+}
---
+++ C# Fundamentals - 2021/Arrays - More Exercise/3. Recursive Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace fibonacci_recursive
+{
+    class Program
+    {
+        static void Main()
+        {
+            int nFibonacci = int.Parse(Console.ReadLine());
+
+            if (nFibonacci <= 1)
+            {
+                Console.WriteLine(1);
+            }
+
+            else
+            {
+
+                double plusPhi = (1 + Math.Sqrt(5)) / 2;
+                double minusPhi = (1 - Math.Sqrt(5)) / 2;
+
+
+                double fibonacci = (Math.Pow(plusPhi, nFibonacci) - Math.Pow((-minusPhi), nFibonacci))
+                    / Math.Sqrt(5);
+
+                long roundedFib = (long)Math.Round(fibonacci);
+
+                Console.WriteLine(roundedFib);
+
+            }
+        }
+    }
+}
