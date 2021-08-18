26b39d96cb14998c37656e1885dbd92d33f94fa7
Miroslav <miroslav_995@abv.bg>
2021-08-18 20:07:10 +0300

    Adding on Defining Classes - Lab

---
+++ C# Advanced - 2021/Defining Classes - Lab/2. Car Extension/Car.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Text;
+
+
+namespace CarManufacturer
+{
+	public class Car
+	{
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+		public double FuelQuantity { get; set; }
+		public double FuelConsumption { get; set; }
+		public void Drive(double distance)
+		{
+			var neededFuel = distance - this.FuelConsumption;
+			bool canContiniue = this.FuelQuantity - (distance * this.FuelConsumption) >= 0;
+			if (canContiniue)
+			{
+				this.FuelQuantity -= distance * this.FuelConsumption;
+			}
+			else
+			{
+				Console.WriteLine("Not enough fuel to perform this trip!");
+			}
+		}
+		public string GetSpecifications()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			sb.AppendLine($"Make: {this.Make}");
+			sb.AppendLine($"Model: {this.Model}");
+			sb.AppendLine($"Year: {this.Year}");
+			sb.Append($"Fuel: {this.FuelQuantity:F2}L");
+			return sb.ToString();
+		}
+
+
+	}
+}
