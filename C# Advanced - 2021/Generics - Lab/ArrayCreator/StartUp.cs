25a2152b8ab304735770fe8b1344e0e6e49b8683
Miroslav <miroslav_995@abv.bg>
2021-08-20 23:44:05 +0300

    Adding on Generics - Lab

---
+++ C# Advanced - 2021/Generics - Lab/ArrayCreator/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine(string.Join(", ",ArrayCreator.Create(10,"pesho")));
+		}
+	}
+}
