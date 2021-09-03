96d1c740e65bf08bb781a0eadb8c961ed852c148
Miroslav <miroslav_995@abv.bg>
2021-09-03 22:26:59 +0300

    Adding on Polymorphism - Lab

---
+++ C# OOP/C# OOP - Exercises/Polymorphism - Lab/02. Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Animals
+{
+	public class Animal
+	{
+		public Animal(string name, string favoriteFood)
+		{
+			this.Name = name;
+			this.FavouriteFood = favoriteFood;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public string FavouriteFood { get => favouriteFood; set => favouriteFood = value; }
+		private string name;
+		private string favouriteFood;
+
+		public virtual string ExplainSelf()
+		{
+			return $"I am {this.name} and my fovourite food is {this.favouriteFood}";
+		}
+	}
+}
---
+++ C# OOP/C# OOP - Exercises/Polymorphism - Lab/03.Shapes/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Shapes
+{
+	public class StartUp
+	{
+		static void Main()
+		{
+			Circle circle = new Circle(2);
+			double circleArea = circle.CalculateArea();
+			double circlePerimeter = circle.CalculatePerimeter();
+
+			Rectangle rectangle = new Rectangle(5, 6);
+			double rectArea = rectangle.CalculateArea();
+			double rectPerimeter = rectangle.CalculatePerimeter();
+
+			Console.WriteLine(circle.Draw());
+			Console.WriteLine($"Circle area: {circleArea:f2}");
+			Console.WriteLine($"Circle perimeter: {circlePerimeter:f2}");
+			
+			Console.WriteLine(rectangle.Draw());
+			Console.WriteLine($"Rectangle area: {rectArea:f2}");
+			Console.WriteLine($"Rectangle primeter: {rectPerimeter:f2}");
+		}
+	}
+}
