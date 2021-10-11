4db81145e66ea84c413dcf4bab13ce1784594962
mmIIrrOOss <miroslav_995@abv.bg>
2021-10-11 18:30:35 +0300

    Adding on Exercise Create Custom List

---
+++ C# Advanced - 2021/Implementing Stack and Queue/Implement the CustomStack/CustomStack.cs
@@ -0,0 +1,60 @@
+﻿namespace Implement_the_CustomStack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class CustomStack
+    {
+        public CustomStack()
+        {
+            this.count = 0;
+            this.Items = new int[InitialCapacity];
+        }
+        public int Count => this.count;
+
+        private const int InitialCapacity = 4;
+        private int[] Items;
+        private int count;
+        public void Push(int element)
+        {
+            if (this.Items.Length == this.Count)
+            {
+                var nextItems = new int[this.Items.Length * 2];
+                for (int i = 0; i < this.Items.Length; i++)
+                {
+                    nextItems[i] = this.Items[i];
+                }
+                this.Items = nextItems;
+            }
+            this.Items[this.count] = element;
+            count++;
+        }
+        public int Pop(int element)
+        {
+            if (this.Items.Length==0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            var lastIndex = this.count - 1;
+            int last = this.Items[lastIndex];
+            this.count--;
+            return last;
+        }
+        public int Peek()
+        {
+            if (this.Items.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            var lastElement = this.Items[this.Items.Length - 1];
+            return lastElement;
+        }
+        public void ForEach(Action<object>  action)
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                action(this.Items[i]);
+            }
+        }
+    }
+}
---
+++ C# Advanced - 2021/Implementing Stack and Queue/Implement the CustomStack/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Implement_the_CustomStack
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            var customStack = new CustomStack();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Pushhh: {i}");
+                customStack.Push(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Pooppp: {i}");
+                customStack.Pop(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Peek method is workkk {customStack.Peek()}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Custom stack is ForEach method:");
+            customStack.ForEach(x => Console.WriteLine($"Is Foreach method ---- {x}"));
+        }
+    }
+}
