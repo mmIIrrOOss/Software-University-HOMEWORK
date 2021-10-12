48d2e3e20432f365d26fc7307ffe9f80acbebb7a
mmIIrrOOss <miroslav_995@abv.bg>
2021-10-12 21:05:34 +0300

    Adding on my Custom Stack-Exercise

---
+++ C# Advanced - 2021/Implementing Stack and Queue/Implementing Stack and Queue/CustomList.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Implementing_Stack_and_Queue
+{
+    using System;
+    using System.Linq;
+    public class CustomList
+    {
+        public CustomList()
+        {
+            this.Items = new int[InitialCapacity];
+        }
+        private const int InitialCapacity = 2;
+        private int[] Items;
+        public int Count { get; private set; }
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return Items[index];
+            }
+            set
+            {
+                if (index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                Items[index] = value;
+            }
+        }
+        private void Resize()
+        {
+            int[] copy = new int[this.Items.Length * 2];
+            for (int i = 0; i < this.Items.Length; i++)
+            {
+                copy[i] = this.Items[i];
+            }
+            this.Items = copy;
+        }
+        public void Add(int item)
+        {
+            if (this.Count == this.Items.Length)
+            {
+                this.Resize();
+            }
+            this.Items[this.Count] = item;
+            this.Count++;
+        }
+        public int RemoveAt(int index)
+        {
+            if (index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            int item = this.Items[index];
+            this.Items[index] = default(int);
+            this.Shift(index);
+
+            this.Count--;
+            if (this.Count <= this.Items.Length / 4)
+            {
+                this.Shrink();
+            }
+            return item;
+        }
+
+        private void Shrink()
+        {
+            int[] copy = new int[this.Items.Length / 2];
+            for (int i = 0; i < this.Count; i++)
+            {
+                copy[i] = this.Items[i];
+            }
+            this.Items = copy;
+        }
+        private void Shift(int index)
+        {
+            for (int i = 0; i < this.Items.Length - 1; i++)
+            {
+                this.Items[i] = this.Items[i + 1];
+            }
+        }
+        private void ShiftToRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                this.Items[i] = this.Items[i - 1];
+            }
+        }
+        public void Insert(int index, int element)
+        {
+            if (index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (this.Count == this.Items.Length)
+            {
+                this.Resize();
+            }
+            this.ShiftToRight(index);
+            this.Items[index] = element;
+            this.Count++;
+        }
+        public bool Contains(int element)
+        {
+            foreach (var item in this.Items)
+            {
+                if (item == element)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Swap(int firstiNdex, int secondIndex)
+        {
+            if (firstiNdex > this.Items.Count() || secondIndex > this.Items.Count())
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            var firstSwapElement = this.Items[firstiNdex];
+            var secondSwapElement = this.Items[secondIndex];
+            this.Items[firstiNdex] = secondSwapElement;
+            this.Items[secondIndex] = firstSwapElement;
+        }
+    }
+}
---
+++ C# Advanced - 2021/Implementing Stack and Queue/Implementing Stack and Queue/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace Implementing_Stack_and_Queue
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            CustomList customList = new CustomList();
+            Console.WriteLine("---Adding elements on my CustomList");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Adding... - {0}", i);
+                customList.Add(i);
+            }
+
+            for (int i = 0; i < customList.Count; i++)
+            {
+                Console.WriteLine("Print ingredients my elements at my CustomList... - {0}", customList[i]);
+            }
+
+
+            Console.WriteLine("Insert this element and indexes.");
+            Console.WriteLine("This moment count of my List {0}", customList.Count);
+            customList.Insert(5, 9);
+            Console.WriteLine($"This moment Count of my List ---{customList.Count}");
+
+            Console.WriteLine("Contans element on my List {0}", customList.Contains(9));
+            Console.WriteLine("Contans element on my List {0}", customList.Contains(100));
+
+            customList.Add(8);
+            int firstIndex = 0;
+            int secondIndex = customList.Count - 1;
+            Console.WriteLine("First swap element - {0}", customList[firstIndex]);
+            Console.WriteLine("Second swap element - {0}", customList[secondIndex]);
+            customList.Swap(firstIndex, secondIndex);
+            Console.WriteLine("Swaping elemnts..........");
+            Console.WriteLine($"Swaping succesfull! View Change down");
+            Console.WriteLine($"{customList[firstIndex]}");
+            Console.WriteLine($"{customList[secondIndex]}");
+            for (int i = 0; i < customList.Count - 1; i++)
+            {
+                Console.WriteLine(customList[i]);
+            }
+
+            Console.WriteLine($"Removed this element - {customList[0]}");
+            customList.RemoveAt(customList[0]);
+
+            Console.WriteLine("END!");
+        }
+    }
+}
