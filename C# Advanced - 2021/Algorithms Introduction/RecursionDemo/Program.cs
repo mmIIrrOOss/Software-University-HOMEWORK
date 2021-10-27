ccdd17fbda0627945f9e9ae1a1a147fa45e8420d
mmIIrrOOss <miroslav_995@abv.bg>
2021-10-27 23:06:08 +0300

    Learning Recursion and Algorithms. Adding the first exercise for recursion.

---
+++ C# Advanced - 2021/Algorithms Introduction/RecursionDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿
+namespace RecursionDemo
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[] { 1, 2, 3 };
+            Traverse(array, 0);
+            //int number = int.Parse(Console.ReadLine());
+            //int pow = int.Parse(Console.ReadLine());
+            //Console.WriteLine(Pow(number, pow));
+        }
+        private static void Traverse<T>(T[] collection, int index)
+        {
+            if (index == collection.Length)
+            {
+                return;
+            }
+            Console.WriteLine(collection[index]);
+            Traverse(collection, index + 1);
+        }
+        private static void Loop(int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+            Console.WriteLine($"Hello {index}");
+            Loop(index--);
+        }
+        private static int Pow(int number, int pow)
+        {
+            if (pow == 1)
+            {
+                return number;
+            }
+            int current = number * Pow(number, pow - 1);
+            return current;
+        }
+    }
+}
