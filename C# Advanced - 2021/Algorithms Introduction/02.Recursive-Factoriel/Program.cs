c01c3bfd9d3b827203a6bee00d498c0a2f862fc3
mmIIrrOOss <miroslav_995@abv.bg>
2021-10-30 15:50:51 +0300

    Adding on Exercise - Recursive-Factoriel

---
+++ C# Advanced - 2021/Algorithms Introduction/02.Recursive-Factoriel/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace _02.Recursive_Factoriel
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factoriel(num));
+        }
+
+        private static int Factoriel(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            int result = num * Factoriel(num - 1);
+            return result;
+        }
+    }
+}
