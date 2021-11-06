30a6c133715d973559ee42832e17f5d78b7df462
mmIIrrOOss <miroslav_995@abv.bg>
2021-11-06 21:04:08 +0200

    Adding more implemented algotithm BubbleSort

---
+++ C# Advanced - 2021/Algorithms Introduction/BubbleSort/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace BubbleSort
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        int tempNumber = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = tempNumber;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
