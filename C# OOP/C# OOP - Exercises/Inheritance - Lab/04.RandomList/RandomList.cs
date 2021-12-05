fc722417bc275dc724ad288299c519f5a02992f7
mmIIrrOOss <miroslav_995@abv.bg>
2021-12-05 14:37:46 +0200

    Update Inheritance - Lab
    Updating exercises on Inheritance

---
+++ C# OOP/C# OOP - Exercises/Inheritance - Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CustomRandomList
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+        public string RandomString()
+        {
+            int index = rnd.Next(0, this.Count);
+            string str = this[index];
+            this.RemoveAt(index);
+            return str;
+        }
+    }
+}
---
+++ C# OOP/C# OOP - Exercises/Inheritance - Lab/05.StackOfString/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace CustomStack
+{
+ 
+    using System.Collections.Generic;
+    
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            bool isEmty = this.Count == 0;
+            return isEmty;
+        }
+        public void AddRange(IEnumerable<string> values)
+        {
+            foreach (var value in values)
+            {
+                this.Push(value);
+            }
+        }
+    }
+}
