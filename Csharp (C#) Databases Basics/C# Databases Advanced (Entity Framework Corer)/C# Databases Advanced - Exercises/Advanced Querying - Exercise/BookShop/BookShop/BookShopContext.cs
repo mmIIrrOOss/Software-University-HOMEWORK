b6cab053cc738055e573b45ac1e3772cc9fdef5c
mmIIrrOOss <miroslav_995@abv.bg>
2022-07-13 19:05:12 +0300

    Adding on Advanced Querying Exercise
    BookShop database created in Code First

---
+++ Csharp (C#) Databases Basics/C# Databases Advanced (Entity Framework Corer)/C# Databases Advanced - Exercises/Advanced Querying - Exercise/BookShop/BookShop/BookShopContext.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using BookShop.Models;
+    using BookShop.Data.EntityConfiguration;
+
+    public class BookShopContext : DbContext
+    {
+        public BookShopContext() { }
+
+        public BookShopContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<BookCategory> BookCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+
+            modelBuilder.ApplyConfiguration(new BookCategoryConfiguration());
+        }
+    }
+
+}
