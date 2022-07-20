9b1812539e9285bb43e56564911a86978231ea83
mmIIrrOOss <miroslav_995@abv.bg>
2022-07-20 09:59:03 +0300

    Addition of FastFoodcore web project finishing some functionality related to controllers and database

---
+++ Csharp (C#) Databases Basics/C# Databases Advanced (Entity Framework Corer)/C# Databases Advanced - Exercises/C# Auto Mapping Objects - Exercise/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,36 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Core.ViewModels.Employees;
+    using FastFood.Core.ViewModels.Orders;
+    using FastFood.Models;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+            
+            //Employee
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x=>x.PositionId, y=>y.MapFrom(s=>s.Id));
+             
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Order
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.OrderId, y => y.MapFrom(s => s.EmployeeId));
+
+            this.CreateMap<CreateOrderInputModel, Order>();
+        }
+    }
+}
