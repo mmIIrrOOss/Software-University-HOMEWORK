d7926483689c6489f256768e0920eca46dbc3e4d
mmIIrrOOss <miroslav_995@abv.bg>
2022-08-09 18:32:22 +0300

    Adding on XML exercise. Practice how to convert from Exemel to a CSharp object and vice versa.

---
+++ Csharp (C#) Databases Basics/C# Databases Advanced (Entity Framework Corer)/C# Databases Advanced - Exercises/XML Processing - Exercise/CarDealer/DTO/Import/SupplierImportDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.Import
+{
+    [XmlType]
+    public class SupplierImportDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("is Importer")]
+        public bool IsImporter { get; set; }
+
+    }
+}
---
+++ Csharp (C#) Databases Basics/C# Databases Advanced (Entity Framework Corer)/C# Databases Advanced - Exercises/XML Processing - Exercise/CarDealer/StartUp.cs
@@ -0,0 +1,77 @@
+﻿namespace CarDealer
+{
+    using CarDealer.DTO.Import;
+    using CarDealer.Models;
+    using Data;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+    using System;
+    using CarDealer.XML_Helper;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext carDealerContext = new CarDealerContext();
+            carDealerContext.Database.EnsureDeleted();
+            carDealerContext.Database.EnsureCreated();
+
+            var suppliersXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            string resultImportSuppliers = ImportSuppliers(carDealerContext, suppliersXml);
+            Console.WriteLine(resultImportSuppliers);
+
+            var partsXml = File.ReadAllText("../../../Datasets/parts.xml");
+            string resultImportParts = ImportParts(carDealerContext, partsXml);
+            Console.WriteLine(resultImportParts);
+
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var root = "Suppliers";
+
+            var suppliersDto = XmlConverter.Deserializer<SupplierImportDto>(inputXml, root);
+
+            var suppliers = suppliersDto.Select(x => new Supplier()
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter
+            })
+            .ToList();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            return $"Successfully imported {suppliers.Count}";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            var root = "Parts";
+            var partsDto = XmlConverter.Deserializer<PartInputDto>(inputXml, root);
+
+            var supplierIds = context.Suppliers
+                .Select(x => x.Id)
+                .ToList();
+
+            var parts = partsDto
+                .Where(x=> supplierIds.Contains(x.SupplierId))
+                .Select(x => new Part()
+                {
+                      Name = x.Name,
+                       Price = x.Price,
+                       Quantity = x.Quantity,
+                       SupplierId = x.SupplierId
+                })
+                .ToList();
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+            return $"Successfully imported {parts.Count}";
+        }
+
+
+    }
+}
