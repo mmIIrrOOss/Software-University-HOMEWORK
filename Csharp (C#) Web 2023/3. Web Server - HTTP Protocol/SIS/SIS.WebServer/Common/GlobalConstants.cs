fb0714a18f22e8a1bfe718008d4b8687943bc118
Miroslav <miroslav_995@abv.bg>
2023-12-17 19:04:53 +0200

    Implementing the methods in the HttpRequest class

--- Csharp (C#) Web 2023/3. Web Server - HTTP Protocol/SIS/SIS.WebServer/Common/GlobalConstants.cs
+++ Csharp (C#) Web 2023/3. Web Server - HTTP Protocol/SIS/SIS.WebServer/Common/GlobalConstants.cs
@@ -1,11 +1,14 @@
 ﻿namespace SIS.WebServer.Common
 {
-    public class GlobalConstants
+    public static class GlobalConstants
     {
-        public string HttpOneProtocolFragment = "HTTP/1.1";
+        public static string HttpOneProtocolFragment = "HTTP/1.1";
+               
+        public static string HostHeaderKey = "Host";
+               
+        public static string HttpNewLine = "\r\n";
 
-        public string HostHeaderKey = "Host";
-
-        public string HttpNewLine = "\r\n";
+        public static string UnSupportedHttpMethodExceptionMessage
+            = "This HTTP Method - {0} is not supported";
     }
 }
--- Csharp (C#) Web 2023/3. Web Server - HTTP Protocol/SIS/SIS.WebServer/Request/HttpRequest.cs
+++ Csharp (C#) Web 2023/3. Web Server - HTTP Protocol/SIS/SIS.WebServer/Request/HttpRequest.cs
@@ -1,7 +1,10 @@
 ﻿using SIS.WebServer.Common;
 using SIS.WebServer.Enums;
 using SIS.WebServer.Headers;
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SIS.WebServer.Request
 {
@@ -14,6 +17,8 @@ namespace SIS.WebServer.Request
             this.FormData = new Dictionary<string, object>();
             this.QueryData = new Dictionary<string, object>();
             this.Headers = new HttpHeaderCollection();
+
+            
         }
 
         public string Path { get; private set; }
@@ -28,7 +33,109 @@ namespace SIS.WebServer.Request
 
         public HttpRequestMethod RequestMethod { get; private set; }
 
+        private bool IsValidRequestLine(string[] requestLines)
+        {
+            if (requestLines.Length != 3
+                || requestLines[2] != GlobalConstants.HttpOneProtocolFragment)
 
+            {
+                return false;
+            }
+            return true;
+        }
 
+        private bool IsValidRequestQueryString(string queryString, string[] queryParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParseRequestMethod(string[] requestString)
+        {
+            HttpRequestMethod method;
+            bool parseResult = HttpRequestMethod.TryParse(requestString[0], out method);
+
+            if (!parseResult)
+            {
+                throw new BadImageFormatException(string.Format
+                    (GlobalConstants
+                            .UnSupportedHttpMethodExceptionMessage, requestString[0]));
+            }
+            this.RequestMethod = method;
+        }
+
+        private void ParseRequestUrl(string[] requestLine)
+        {
+            this.Url = requestLine[1];
+        }
+        
+        private void ParseRequestPath()
+        {
+            this.Path = this.Url.Split('?')[0];
+        }
+
+        private void ParseRequestHeaders(string[] requestHeaders)
+        {
+            requestHeaders
+                .Select(x => x.Split(new[] { ',', ':' }, StringSplitOptions.RemoveEmptyEntries))
+                .ToList()
+                .ForEach(headersKeyValuePair => this.Headers
+                                            .AddHeader(new HttpHeader( 
+                                            headersKeyValuePair[0], headersKeyValuePair[1])));
+        }
+
+        private void ParseRequestCookies()
+        {
+
+        }
+
+        private void ParseRequestQueryParameters()
+        {
+            this.Url.Split('?')[1]
+                .Split('&')
+                .Select(qm => qm.Split('='))
+                .ToList()
+                .ForEach(parseQueryParameter => 
+                this.QueryData.Add(parseQueryParameter[0], parseQueryParameter[1]));
+        }
+
+        private void ParseRequestFormDataParameters(string formData)
+        {
+            formData.Split('&')
+                .Select(qm => qm.Split('='))
+                .ToList()
+                .ForEach(parseQueryParameter =>
+                this.FormData.Add(parseQueryParameter[0], parseQueryParameter[1]));
+        }
+
+        private void ParseRequestParameters(string formData)
+        {
+            this.ParseRequestQueryParameters();
+            this.ParseRequestFormDataParameters(formData);
+        }
+
+        private void ParseRequest(string requestString)
+        {
+            string[] splitRequestContent = requestString
+                .Split(new[] { GlobalConstants.HttpNewLine }, System.StringSplitOptions.None);
+
+            string[] requestLine = splitRequestContent[0]
+                .Trim()
+                .Split(new[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (!this.IsValidRequestLine(requestLine))
+            {
+                throw new BadImageFormatException();
+            }
+
+            this.ParseRequestMethod(requestLine);
+            this.ParseRequestUrl(requestLine);
+            this.ParseRequestPath();
+
+            this.ParseHeaders(splitRequestContent.Skip(1).ToArray());
+            this.ParseCookies();
+
+            this.ParseRequestParameters(splitRequestContent[splitRequestContent.Length - 1]);
+
+        }
     }
 }
